3f1d4dbab4bd368d2cc6ee50a13b7f48d26936e9
Pzqqt <821026875@qq.com>
2019-01-18 22:03:44 +0800

    Show different icons depending on the module status

---
+++ script/gen-icons-prop.sh
@@ -0,0 +1,26 @@
+#!/sbin/sh
+
+dst_prop_file=/tmp/aroma/module_icon.prop
+
+rm -f $dst_prop_file
+touch $dst_prop_file
+
+for module in `cat /tmp/mmr/script/modules_ids`; do
+  module_status=`/tmp/mmr/script/control-module.sh status $module; echo $?`
+  # Enable: 1, Disable: 0, Removed: 2, UpdateFlag: 3, RemoveFlag: 4
+  case $module_status in
+    1) useicon="@default"
+    ;;
+    0) useicon="@disable"
+    ;;
+    2) useicon="@removed"
+    ;;
+    3) useicon="@updateflag"
+    ;;
+    4) useicon="@removeflag"
+    ;;
+    *) useicon="@default"
+    ;;
+  esac
+  echo "module.icon.${module}=${useicon}" >> $dst_prop_file
+done
