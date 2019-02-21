2265dedb6b1157302c786abbce66c0cf4123e335
Pzqqt <821026875@qq.com>
2019-02-21 18:37:43 +0800

    Use an easier way to create symbolic link Now easier to operate.

--- script/symlink-magisk.sh
+++ script/symlink-magisk.sh
@@ -10,25 +10,14 @@ is_mounted() { mountpoint -q "$1"; }
 symlink_modules() {
     mount -o remount,rw /
     rm -rf $2
-    mkdir -p $2
-    is_mounted $2 && {
-        loopedA=`mount | grep $2 | head -n1 | cut -d " " -f1`
-        umount $2
-        losetup -d $loopedA
-    }
-    installed_modules=`ls -1 $1 | grep -v 'lost+found'`
-    if [ ${#installed_modules} -ne 0 ]; then
-        for module in ${installed_modules}; do
-            ln -s ${1}/${module} $2
-        done
-    fi
+    ln -s $1 $2
 }
 
 gen_done_script() {
     cat > $donescript <<EOF
 #!/sbin/sh
 
-rm -rf /magisk
+rm -rf $workPath
 
 EOF
     chmod 0755 $donescript
