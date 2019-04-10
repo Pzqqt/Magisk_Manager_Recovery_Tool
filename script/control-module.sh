2c538cff924c456f99a0bc2ba3e1b16a5555fab2
Pzqqt <821026875@qq.com>
2019-04-11 01:23:21 +0800

    Rewrite control-module.sh again

--- script/control-module.sh
+++ script/control-module.sh
@@ -6,87 +6,67 @@ module=$2
 workPath=/magisk
 modulePath=${workPath}/${module}
 
-touch_flag() { touch ${1}/${2} || { cd ${1}? && touch ./${2}; }; }
+exist_flag() { [ -f ${modulePath}/${1} ]; }
 
-case $operate in
+rm_flag() { rm -f ${modulePath}/${1} && echo ${2} && exit 0; }
+
+touch_flag() {
+    touch ${modulePath}/${1} || { cd ${modulePath}? && touch ./${1}; }
+    [ $? = 0 ] && echo ${2} && exit 0
+}
+
+[ -n $operate ] && [ -n $module ] && case $operate in
     "status") {
         # Enable: 1, Disable: 0, Removed: 2, UpdateFlag: 3,
         # RemoveFlag & Enable: 4, RemoveFlag & Disable: 5
-        [ ! -d $modulePath ] && exit 2
-        [ -f $modulePath/update ] && exit 3
-        if [ -f $modulePath/remove ]; then
-            [ -f $modulePath/disable ] && exit 5 || exit 4
+        [ -d $modulePath ] || exit 2
+        exist_flag "update" && exit 3
+        if exist_flag "remove"; then
+            exist_flag "disable" && exit 5 || exit 4
         else
-            [ -f $modulePath/disable ] && exit 0 || exit 1
+            exist_flag "disable" && exit 0 || exit 1
         fi
     } ;;
     "status_auto_mount") {
         # Enable: 1, Disable: 0, Removed: 2
-        [ ! -d $modulePath ] && exit 2
-        [ -f $modulePath/auto_mount ] && exit 1 || exit 0
+        [ -d $modulePath ] || exit 2
+        exist_flag "auto_mount" && exit 1 || exit 0
     } ;;
     "status_skip_mount") {
         # Enable: 1, Disable: 0, Removed: 2
-        [ ! -d $modulePath ] && exit 2
-        [ -f $modulePath/skip_mount ] && exit 0 || exit 1
+        [ -d $modulePath ] || exit 2
+        exist_flag "skip_mount" && exit 0 || exit 1
     } ;;
     "switch_module") {
-        if [ -f $modulePath/disable ]; then
-            rm -rf $modulePath/disable && {
-                echo "Successfully enable module $module !"
-                exit 0
-            }
+        if exist_flag "disable"; then
+            rm_flag "disable" "Successfully enable module $module !"
         else
-            touch_flag $modulePath disable && {
-                echo "Successfully disable module $module !"
-                exit 0
-            }
+            touch_flag "disable" "Successfully disable module $module !"
         fi
     } ;;
     "switch_auto_mount") {
-        if [ -f $modulePath/auto_mount ]; then
-            rm -rf $modulePath/auto_mount && {
-                echo "Successfully disable mount for $module !"
-                exit 0
-            }
+        if exist_flag "auto_mount"; then
+            rm_flag "auto_mount" "Successfully disable mount for $module !"
         else
-            touch_flag $modulePath auto_mount && {
-                echo "Successfully enable mount for $module !"
-                exit 0
-            }
+            touch_flag "auto_mount" "Successfully enable mount for $module !"
         fi
     } ;;
     "switch_skip_mount") {
-        if [ -f $modulePath/skip_mount ]; then
-            rm -rf $modulePath/skip_mount && {
-                echo "Successfully enable mount for $module !"
-                exit 0
-            }
+        if exist_flag "skip_mount"; then
+            rm_flag "skip_mount" "Successfully enable mount for $module !"
         else
-            touch_flag $modulePath skip_mount && {
-                echo "Successfully disable mount for $module !"
-                exit 0
-            }
+            touch_flag "skip_mount" "Successfully disable mount for $module !"
         fi
     } ;;
     "switch_remove") {
-        if [ -f $modulePath/remove ]; then
-            rm -rf $modulePath/remove && {
-                echo "Successful undo."
-                exit 0
-            }
+        if exist_flag "remove"; then
+            rm_flag "remove" "Successful undo."
         else
-            touch_flag $modulePath remove && {
-                echo "Module $module will be removed at next reboot."
-                exit 0
-            }
+            touch_flag "remove" "Module $module will be removed at next reboot."
         fi
     } ;;
     "remove") {
-        rm -rf $modulePath && {
-            echo "Successfully removed module $module !"
-            exit 0
-        }
+        rm -rf $modulePath && echo "Successfully removed module $module !" && exit 0
     } ;;
     *) {
         echo -e "\nUnknown operation: $operate"
