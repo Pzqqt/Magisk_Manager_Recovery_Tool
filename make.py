3251d3fa074a7d10e1b0dc10ab6035a615e847f3
Pzqqt <821026875@qq.com>
2019-04-13 22:11:39 +0800

    v1.9 Alpha

--- make.py
+++ make.py
@@ -7,7 +7,7 @@ import shutil
 import zipfile
 import time
 
-LOCALVERSION = "v1.8"
+LOCALVERSION = "v1.9"
 
 INCLUDE_DIRS = ("META-INF", "script", "template")
 INCLUDE_FILES = ("LICENSE", "README.md")
