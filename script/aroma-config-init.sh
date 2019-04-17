639d2ae4d4b0dfc099ffc7d7b00c3c4e84ea3f1c
Pzqqt <821026875@qq.com>
2019-04-17 20:42:59 +0800

    Show core only mode warning at Superuser menu too

--- script/aroma-config-init.sh
+++ script/aroma-config-init.sh
@@ -304,7 +304,7 @@ if prop("operations.prop", "selected") == cal("$i", "+", "1") then
     if prop("advanced.prop", "selected") == "4" then
         menubox(
             "Superuser",
-            "Choose an action",
+            "Choose an action" + getvar("core_only_mode_warning"),
             "@welcome",
             "magisksu.prop",
             "Clear MagiskSU logs", "", "@action",
