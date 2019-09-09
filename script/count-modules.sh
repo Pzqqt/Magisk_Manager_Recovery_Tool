b93ae2b3b0002f2c7f411a12936d83e98a9023d2
Pzqqt <821026875@qq.com>
2019-09-09 10:15:05 +0800

    count-modules.sh: no newline

--- script/count-modules.sh
+++ script/count-modules.sh
@@ -2,4 +2,4 @@
 
 . /tmp/mmr/script/common.sh
 
-echo "\n你已安装 $(ls_mount_path | wc -l) 个模块, 总占用空间: $(du -sh ${workPath}/ | awk '{print $1}')"
+echo -e "\n你已安装 $(ls_mount_path | wc -l) 个模块, 总占用空间: $(du -sh ${workPath}/ | awk '{print $1}')\c"
