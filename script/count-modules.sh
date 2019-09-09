617a1bfe5cd93776a10b5d4a7630f036c26a30c7
Pzqqt <821026875@qq.com>
2019-09-09 10:15:05 +0800

    count-modules.sh: no newline

--- script/count-modules.sh
+++ script/count-modules.sh
@@ -2,4 +2,4 @@
 
 . /tmp/mmr/script/common.sh
 
-echo "\nYou have installed $(ls_mount_path | wc -l) module(s), Total size: $(du -sh ${workPath}/ | awk '{print $1}')"
+echo -e "\nYou have installed $(ls_mount_path | wc -l) module(s), Total size: $(du -sh ${workPath}/ | awk '{print $1}')\c"
