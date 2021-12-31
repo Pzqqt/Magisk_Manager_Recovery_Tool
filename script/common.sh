fb84900202117df9ed020e6807afd258429f7403
Pzqqt <821026875@qq.com>
2022-01-01 00:07:23 +0800

    Compatible with Magisk 23010+
    - Add an option to enable/disable Zygisk
    - Add an option to enable/disable Deny List
    - Disable MagiskHide switch if Magisk version number >= 23010
    - Move Magisk Core Only Mode, MagiskHide, Zygisk and Deny List to the new "Magisk Settings" menu

--- script/common.sh
+++ script/common.sh
@@ -4,15 +4,14 @@ echo $PATH | grep -q "^/tmp/bb" || export PATH=/tmp/bb:${PATH}
 
 workPath=/magisk
 module_backup_path=/sdcard/TWRP/magisk_module_backup
+magisk_db=/data/adb/magisk.db
 
 file_getprop() { grep "^$2=" "$1" | head -n1 | cut -d= -f2; }
-
 _ls() { ls -1 ${1} | grep -v 'lost+found'; }
-
 ls_mount_path() { _ls ${workPath}; }
-
 ls_module_backup_path() { _ls ${module_backup_path}; }
 
-magisk_db=/data/adb/magisk.db
+MAGISK_VER=$(file_getprop /data/adb/magisk/util_functions.sh MAGISK_VER)
+MAGISK_VER_CODE=$(file_getprop /data/adb/magisk/util_functions.sh MAGISK_VER_CODE)
 
 # Append lines below
